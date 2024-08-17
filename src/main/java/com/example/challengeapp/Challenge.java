1612f880a47a8948a2820ed4ec7987531ea6a7d1
Jason <extremewinds@gmail.com>
2024-08-17 14:33:57 +0200

    add db support

--- src/main/java/com/example/challengeapp/Challenge.java
+++ src/main/java/com/example/challengeapp/Challenge.java
@@ -1,13 +1,23 @@
 package com.example.challengeapp;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Entity
 public class Challenge {
+    // SQL query for retrieving columns
+    // SELECT * from infoRMATION_SCHEMA.cOLUMNS whERE tabLE_NAME = 'CHALLENGE'
+    @Id
     private Long id;
-    private String month;
+    @Column(name = "challengeMonth")
+    private String month; // the month keyword is reserved in db
     private String description;
 }
