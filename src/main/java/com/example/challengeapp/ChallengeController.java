cda62c53b5794a0a2462c03f320087d4e1a54e4c
Jason <extremewinds@gmail.com>
2024-08-24 15:29:23 +0200

    add swagger ui to the project with 2 documented endpoints

--- src/main/java/com/example/challengeapp/ChallengeController.java
+++ src/main/java/com/example/challengeapp/ChallengeController.java
@@ -1,12 +1,28 @@
 package com.example.challengeapp;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Tag(name = "Challenges API", description = "This API enables you to manage the challenges in the DB")
 @RequestMapping("/api/v1/challenges")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ChallengeController {
@@ -18,11 +34,41 @@ public class ChallengeController {
     }
 
     @GetMapping
+    @Operation(summary = "Retrieve all Challenges", tags = {"in development"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(type = "array", implementation = Challenge.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     private ResponseEntity<List<Challenge>> getAllChallenges() {
         return new ResponseEntity<>(challengeService.getAllChallenges(), HttpStatus.OK);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new Challenge object", tags = {"in development"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = {
+                    @Content(schema = @Schema(implementation = Challenge.class), mediaType = "application/json", examples = {
+                            @ExampleObject(
+                                    name = "Add challenge",
+                                    summary = "Challenge added successfully",
+                                    value = "{\"id\": 1, \"month\": \"January\", \"description\": \"Skiing\"}"
+                            )
+                    })
+            }),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Examples of creating a new challenge",
+            required = true,
+            content = {
+                    @Content(schema = @Schema(implementation = Challenge.class), examples = {
+                            @ExampleObject(
+                                    name = "A new challenge should be created",
+                                    summary = "201 Example",
+                                    value = "{\"month\": \"January\", \"description\": \"Skiing\"}"
+                            )
+                    })
+            })
     private ResponseEntity<String> addChallenge(@RequestBody Challenge challenge) {
         boolean isChallengeAdded = challengeService.addChallenge(challenge);
         if(isChallengeAdded) {
