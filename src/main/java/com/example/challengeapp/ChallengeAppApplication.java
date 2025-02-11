bc7f8c5d9ee658d52be5f6c3c63c614752db914c
Jason <extremewinds@gmail.com>
2025-02-11 23:34:31 +0100

    Add a test restClient and remove the react profile so the angular could be the default

--- src/main/java/com/example/challengeapp/ChallengeAppApplication.java
+++ src/main/java/com/example/challengeapp/ChallengeAppApplication.java
@@ -1,7 +1,11 @@
 package com.example.challengeapp;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class ChallengeAppApplication {
@@ -9,5 +13,23 @@ public class ChallengeAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChallengeAppApplication.class, args);
     }
+
+    @Bean
+    RestClient restClient(RestClient.Builder builder) {
+        return builder.baseUrl("https://jsonplaceholder.typicode.com/todos/").build();
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(RestClient restClient) {
+        return args -> {
+            ResponseEntity<Todo> entity = restClient.get()
+                    .uri("/{id}", 10)
+                    .retrieve()
+                    .toEntity(Todo.class);
+            System.out.println(entity.getBody());
+        };
+    }
+
+    record Todo(Long id, Long userId, String title) {}
 
 }
